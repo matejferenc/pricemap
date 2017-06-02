8a45ef208daede05b0077f8e5fd86f91c2f2b939
Matej Ferenc <matej.ferenc@monster.com>
2017-06-02 18:05:56 +0200

    Trying to speed up PriorityQueue

--- core/src/main/java/com/averagemap/core/valueCalculator/InverseDistanceWeighting.java
+++ core/src/main/java/com/averagemap/core/valueCalculator/InverseDistanceWeighting.java
@@ -1,24 +1,26 @@
 package com.averagemap.core.valueCalculator;
 
+import com.averagemap.core.FastPriorityQueue;
+import com.averagemap.core.coordinates.distance.Distance;
 import com.averagemap.core.coordinates.model.GoogleMapsPosition;
 import com.averagemap.core.coordinates.model.Point;
-import com.averagemap.core.coordinates.distance.Distance;
 import javafx.util.Pair;
 
+import java.util.ArrayList;
 import java.util.Collection;
-import java.util.PriorityQueue;
+import java.util.List;
 
 public class InverseDistanceWeighting implements PointValueCalculator {
 
     private final Distance distance;
 
-    private final Collection<Point<GoogleMapsPosition>> points;
+    private final List<Point<GoogleMapsPosition>> points;
 
     public final static int K = 10;
 
     public InverseDistanceWeighting(Distance distance, Collection<Point<GoogleMapsPosition>> points) {
         this.distance = distance;
-        this.points = points;
+        this.points = new ArrayList<>(points);
     }
 
     @Override
@@ -30,22 +32,29 @@ public class InverseDistanceWeighting implements PointValueCalculator {
             }
         }
 
-        PriorityQueue<Pair<Double, Double>> closestPoints = new PriorityQueue<>((Pair<Double, Double> o1, Pair<Double, Double> o2) -> o2.getKey().compareTo(o1.getKey()));
-        points.forEach(point -> {
+        FastPriorityQueue<Pair<Double, Double>> closestPoints = new FastPriorityQueue<>(K + 1, (Pair<Double, Double> o1, Pair<Double, Double> o2) -> o2.getKey().compareTo(o1.getKey()));
+
+        for(int i = 0; i < K; i++) {
+            Point<GoogleMapsPosition> point = points.get(i);
             double distance = this.distance.distance(point.getPosition(), pixelPosition);
             if (new Double(distance).equals(Double.NaN)) {
-                return;
+                continue;
             }
-//            distance *= distance;
-            if (closestPoints.size() >= K) {
-                if (closestPoints.peek().getKey() > distance) {
-                    closestPoints.poll();
-                    closestPoints.add(new Pair<>(distance, point.getValue()));
-                }
-            } else {
-                closestPoints.add(new Pair<>(distance, point.getValue()));
+            closestPoints.offer(new Pair<>(distance, point.getValue()));
+        }
+
+        for(int i = K; i < points.size(); i++) {
+            Point<GoogleMapsPosition> point = points.get(i);
+            double distance = this.distance.distance(point.getPosition(), pixelPosition);
+            if (new Double(distance).equals(Double.NaN)) {
+                continue;
             }
-        });
+            if (closestPoints.peek().getKey() > distance) {
+                closestPoints.poll();
+                closestPoints.offer(new Pair<>(distance, point.getValue()));
+            }
+        }
+
         double sumOfWeights = closestPoints.stream().mapToDouble(pair -> 1 / pair.getKey()).sum();
         return closestPoints.stream()
                 .mapToDouble(pair -> (1 / pair.getKey()) * pair.getValue())
